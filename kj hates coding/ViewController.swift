f957f5c9ed643016e4aaa59d31978056075e7662
Kajae Langlet <kajaelangley@gmail.com>
2021-09-24 08:38:37 -0400

    Initial Commit

---
+++ kj hates coding/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  kj hates coding
+//
+//  Created by Kajae Langley on 9/24/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
